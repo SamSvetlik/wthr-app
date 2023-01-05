922fbb81e46078f946242cb49c5964ab4cf64ace
SamSvetlik <sam.svetlik@gmail.com>
2023-01-04 21:31:19 -0600

    chore: initial weatherAPI fetch & store, WeatherDisplay component receives correct props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react'
 import Menu from './components/Menu';
 import Login from './components/Login';
 import WeatherDisplay from './components/WeatherDisplay';
 
 function App() {
+
+  const [current, setCurrent] = useState({})
+  const [forecast, setForecast] = useState({})
+  const [location, setLocation] = useState({name: "Lubbock"})
+  
+
+  useEffect(()=> {
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${location.name}`)
+      .then(res => res.json())
+      .then((data)=> {
+        console.log(data)
+        setCurrent(data.current)
+        setForecast(data.forecast)
+        setLocation(data.location)
+      })
+      .catch(err => console.log(`Error: ${err}`))
+  }, [])
+
   return (
     <div className="App">
       <Menu />
       <Login />
-      <WeatherDisplay />
+      <WeatherDisplay current={current} forecast={forecast} location={location}/>
     </div>
   );
 }
--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,10 +1,10 @@
 const WeatherDisplay = (props) => {
-
+    const { current, forecast, location } = props
     return(
         <>
-        <h3>Today</h3>
-        <h3>Now</h3>
-        <h3>Later</h3>
+        <h3>Today: {forecast.forecastday[0].day.maxtemp_f}</h3>
+        <h3>Now: {forecast.forecastday[0].hour[0].feelslike_f}</h3>
+        <h3>Later: {forecast.forecastday[0].hour[4].feelslike_f}</h3>
         </>
     )
 }
