1d0227885062c965c34d483aed1f24d821d4daf7
SamSvetlik <sam.svetlik@gmail.com>
2023-01-06 16:00:21 -0600

    bugfix: Now and Later in WeatherDisplay now show correct temps, though Later will return undefined if later than 8 pm.

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,10 +1,14 @@
 const WeatherDisplay = (props) => {
     const { current, forecast, location } = props
+
+        let today = new Date()
+        let now = today.getHours()
+    
     return(
         <>
         <h3>Today: {forecast.forecastday[0].day.maxtemp_f}</h3>
-        <h3>Now: {forecast.forecastday[0].hour[0].feelslike_f}</h3>
-        <h3>Later: {forecast.forecastday[0].hour[4].feelslike_f}</h3>
+        <h3>Now: {forecast.forecastday[0].hour[now].feelslike_f}</h3>
+        <h3>Later: {forecast.forecastday[0].hour[now + 4].feelslike_f}</h3>
         </>
     )
 }
